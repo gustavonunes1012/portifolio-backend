f677c1c806676468ceddf155035c05b663533d22
= <=>
2026-03-04 13:41:32 -0300

    tipos primitivos e saida de dados

---
+++ aulas/fundamentos/tipos-primitivos.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome.'))
+idade = int(input('Digite sua idade.'))
+peso = float(input('Digite seu peso em KG'))
+altura= float(input('Digite sua altura em metros.'))
+
+print(f'Meu nome é: {nome}, tenho {idade} anos de idade, tenho {altura} de altura e peso {peso} kilos!')
