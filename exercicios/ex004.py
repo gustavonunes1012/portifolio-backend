9fe60444fc2ee022264f53a64ec216746bd33d48
= <=>
2026-03-04 14:59:28 -0300

    exercicio antecessor e sucessor

---
+++ exercicios/ex004.py
@@ -0,0 +1,7 @@
+ant = int(input('Digite um numero para saber o antecessor:'))
+menos = ant - 1
+print(f'O antecessor de {ant} é: {menos}')
+
+suc = int(input('Digite um numero para saber o sucessor:'))
+mais = suc + 1
+print(f'O sucessor de {suc} é: {mais}')
