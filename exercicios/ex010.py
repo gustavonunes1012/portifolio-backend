9f994833afd60ab91c3f1e2396f279e07af82f3e
= <=>
2026-03-04 16:09:07 -0300

    exercicio maior e menor número

---
+++ exercicios/ex010.py
@@ -0,0 +1,19 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+maiorNumero = n1
+menorNumero = n1
+
+if n2 > maiorNumero:
+    maiorNumero = n2
+if n3 > maiorNumero:
+    maiorNumero = n3
+
+if n2 < menorNumero:
+    menorNumero = n2
+if n3 < menorNumero:
+    menorNumero = n3
+
+print(f'O maior número é: {maiorNumero}')
+print(f'O menor número é: {menorNumero}')
