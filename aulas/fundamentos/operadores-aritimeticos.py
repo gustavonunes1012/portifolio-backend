46ac27dea74078032bb2646d63a18c451f71421b
= <=>
2026-03-04 14:50:26 -0300

    aprendizado operadores aritiméticos

---
+++ aulas/fundamentos/operadores-aritimeticos.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+sub = n1 -n2
+print(f'Os números são: {n1} e {n2}')
+print(f'A soma é: {s}')
+print(f'A multiplicação é: {m}')
+print(f'A divisão é: {d}')
+print(f'A subtração é {sub}')
