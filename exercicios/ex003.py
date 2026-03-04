9fe78db60b61c47d2c92c36b95ca7d1508ac8278
= <=>
2026-03-04 13:57:14 -0300

    somando numeros e melhorando entrada com print formatado

---
+++ exercicios/ex003.py
@@ -0,0 +1,12 @@
+
+numero1 = int(input('Digite o primeiro número:'))
+numero2 = int(input('Digite o segundo número:'))
+soma = numero1 + numero2
+
+print(f'A soma de {numero1} + {numero2} é: {soma}')
+
+numero1 = float(input('Digite o primeiro numero de ponto flutuante:'))
+numero2 = float(input('Digite o segundo número de ponto flutuante:'))
+soma = numero1 + numero2
+
+print(f'A soma dos numeos {numero1} + {numero2} jé formatado é: {soma:.2f}')
