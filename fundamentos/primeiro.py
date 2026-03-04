1c79518d380c1d20a53783507d0976a211dc3b9d
= <=>
2026-03-04 12:43:51 -0300

    aprendendo print, concatenação, variavel e comendo de entrada input

---
+++ fundamentos/primeiro.py
@@ -0,0 +1,16 @@
+print('Ola mundo!')
+
+print(7 + 4)
+
+print('7','4')
+
+nome = 'Gustavo'
+idade = 30
+peso = 100
+
+print(nome,idade,peso)
+
+nome = (input('Qual seu nome?'))
+idade = (input('Qual sua idade?'))
+peso = (input('Qual seu peso?'))
+print(nome, idade, peso)
