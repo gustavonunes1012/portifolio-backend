9eb79e37dbdec66d4b48b0d81efd26f5a07c1b8f
= <=>
2026-03-04 15:31:12 -0300

    condicionais

---
+++ aulas/fundamentos/condicionais.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um número:'))
+if (n > 0) :
+    print(f'O número {n} é maior que 0')
+
+elif (n < 0) :
+    print(f'O número {n} e menor que 0')
+
+else :
+    print('O número é 0')
